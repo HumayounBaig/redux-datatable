8a69f4c1b88e88a2f28557d51591cbb61a196be2
Humayoun Baig <humayoun.mani@gmail.com>
2022-02-22 17:20:53 +0500

    added home route

--- src/App.tsx
+++ src/App.tsx
@@ -3,14 +3,22 @@ import {
   BrowserRouter as Router,
   Routes,
   Route,
+  Link,
 } from "react-router-dom";
 import { Datatable } from './components/datatable';
+
+const Home: React.FC = () => (
+  <Link to="/orders">
+    View Orders
+  </Link>
+)
 
 function App() {
   return (
     <div className="App">
       <Router>
         <Routes>
+          <Route path='/' element={<Home />} />
           <Route path='orders' element={<Datatable />} />
         </Routes>
       </Router>
